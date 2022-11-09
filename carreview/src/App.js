8b72284a38af9013eef9ce2291b5326c4b3150fa
ezra.kipchumba@student.moringaschool.com <ezra.kipchumba@student.moringaschool.com>
2022-11-09 08:07:14 +0300

    add routes

--- carreview/src/App.js
+++ carreview/src/App.js
@@ -1,10 +1,21 @@
 
-import './App.css';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Homepage from "./Homepage";
+import Footer from "./Footer";
 
 function App() {
   return (
     <div className="App">
-      
+      <Router>
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component={Homepage} />
+          </Switch>
+        </div>
+        <Footer />
+      </Router>
     </div>
   );
 }
